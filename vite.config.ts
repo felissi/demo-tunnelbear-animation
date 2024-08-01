8437bbf4af0595f618a98be13220db4f025d8cf6
felissi <felissistarry@gmail.com>
2024-08-01 10:52:51 +0800

    ci: add webpack-stats and relative-ci bundle stats action

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,14 @@ import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 import { fileURLToPath } from "node:url";
 import * as path from "node:path";
+import webpackStatsPlugin from "rollup-plugin-webpack-stats";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), //
+    webpackStatsPlugin(),
+  ],
   css: { postcss: { plugins: [tailwindcss(), autoprefixer()] } },
   resolve: {
     alias: {
@@ -16,4 +20,15 @@ export default defineConfig({
     },
   },
   base: "./",
+  build: {
+    rollupOptions: {
+      output: {
+        // Use a supported file pattern for Vite 5/Rollup 4
+        // @doc https://relative-ci.com/documentation/guides/vite-config
+        assetFileNames: "assets/[name].[hash][extname]",
+        chunkFileNames: "assets/[name].[hash].js",
+        entryFileNames: "assets/[name].[hash].js",
+      },
+    },
+  },
 });
